39c8fcade1a7e50c05698cb5de49733149b0b9eb
Michal Barski <michal16292@gmail.com>
2020-04-05 11:36:55 +0300

    added models

--- event_manager/event/models.py
+++ event_manager/event/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -10,3 +11,11 @@ class Event(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class UserProfileInfo(models.Model):
+
+    user = models.OneToOneField(User)
+
+    def __str__(self):
+        return self.user.username
