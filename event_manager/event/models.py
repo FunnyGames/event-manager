405042f51e500f8773487bcefda81d24e145f7c2
Dima Zagorodny <fgmmaker@gmail.com>
2020-04-16 19:44:20 +0300

    Added fictive events db

--- event_manager/event/models.py
+++ event_manager/event/models.py
@@ -7,7 +7,12 @@ from django.contrib.auth.models import User
 class Event(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
-    date = models.DateTimeField(auto_now=True)
+    start_date = models.DateTimeField(auto_now=False, null=True)
+    end_date = models.DateTimeField(auto_now=False, null=True)
+    capacity = models.IntegerField(default=0)
+    place = models.CharField(max_length=120, default="-")
+    cancelled = models.BooleanField(default=False)
+    create_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
