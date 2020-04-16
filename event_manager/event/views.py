83628b3b949b7f29ffe0ffef978afd9542713357
saard <saardi@ac.sce.ac.il>
2020-04-16 21:59:27 +0300

    PM2020T26-98 add cancelled event model

--- event_manager/event/views.py
+++ event_manager/event/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from .models import Event
+from .models import CancelledEvent
+
+# Create your views here.
+
+Event = Event.objects.all()
+CancelledEvent = CancelledEvent.objects.all()
 
 def home(request):
     context = {
-        'events': Event
+        'events': Event.exclude(id__in=CancelledEvent)
     }
     return render(request, 'event/home.html', context)
 
