5ba5112aa15657351a1f13dd3151e4f051807125
rotem27296 <rotem27296@gmail.com>
2020-05-14 17:38:26 +0300

    PM2020T26-130 recommend event model test

--- event_manager/event/tests/test_models.py
+++ event_manager/event/tests/test_models.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from ..models import Event, CancelledEvent, EventUpdates, RateEvent, MyEvent, EventComment, ReportComment
+from ..models import Event, CancelledEvent, EventUpdates, RateEvent, MyEvent, EventComment, ReportComment,EventRecommend
 from django.contrib.auth.models import User
 
 import this
@@ -125,7 +125,7 @@ class EventCommentTest:
         # Set up non-modified objects used by all test methods
         # Run once to set up non-modified data for all class methods.
 
-        RateEvent.objects.create(
+        EventComment.objects.create(
             EventId=2,
             user=User.objects.create(
                 username='test1', email='test@email.com', first_name='Big', last_name='Bob'),
@@ -134,12 +134,37 @@ class EventCommentTest:
         )
 
     def test_EventId_default(self):
-        event = RateEvent.objects.get(id=1)
+        event = EventComment.objects.get(id=1)
         default = event._meta.get_field('EventId').default
         self.assertEquals(default, 0)
 
     def test_create_date_auto_now(self):
-        event = RateEvent.objects.get(id=1)
+        event = EventComment.objects.get(id=1)
+        auto = event._meta.get_field('create_date').auto_now
+        self.assertEquals(auto, True)
+
+
+class EventRecommendTest:
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        # Run once to set up non-modified data for all class methods.
+
+        EventRecommend.objects.create(
+            EventId=2,
+            user=User.objects.create(
+                username='test1', email='test@email.com', first_name='Big', last_name='Bob'),
+            text="testing"
+
+        )
+
+    def test_EventId_default(self):
+        event = EventRecommend.objects.get(id=1)
+        default = event._meta.get_field('EventId').default
+        self.assertEquals(default, 0)
+
+    def test_create_date_auto_now(self):
+        event = EventRecommend.objects.get(id=1)
         auto = event._meta.get_field('create_date').auto_now
         self.assertEquals(auto, True)
 
