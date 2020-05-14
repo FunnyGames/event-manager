7625e2c09e8c56077ac2378b8bb4071ede4c4a5a
Dima Zagorodny <fgmmaker@gmail.com>
2020-05-14 22:29:24 +0300

    Added user tests

--- event_manager/users/views.py
+++ event_manager/users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, permission_required
 from .forms import UserRegisterForm
 from django.contrib.auth.models import User
 from django.core.paginator import Paginator
@@ -30,6 +30,7 @@ def profile(request):
 
 
 @login_required
+@permission_required('is_superuser')
 def users(request):
     page = request.GET.get('page')
     limit = request.GET.get('limit')
