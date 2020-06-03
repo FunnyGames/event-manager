c4da6ac8970e44f0b3ad96addc64bf76fdf82908
Michal Barski <michal16292@gmail.com>
2020-06-03 19:50:38 +0300

    PM2020T26-139 Created tests for users attendencies

--- event_manager/event/tests/test_views.py
+++ event_manager/event/tests/test_views.py
@@ -168,3 +168,58 @@ class CalendarTest(TestCase):
     def test_view_url_if_not_logged_in(self):
         response = self.client.get(reverse('event-calendar'))
         self.assertEqual(response.status_code, 302)
+
+
+class AttendViewTest(TestCase):
+    def setUp(self):
+        test_event_1 = Event.objects.create(
+            title='Event 1',
+            description='Desc 1',
+            start_date='2000-01-01 10:00',
+            end_date='2000-01-01 12:00',
+            capacity=100,
+            place='tel-aviv'
+        )
+        test_event_1.save()
+
+        test_event_2 = Event.objects.create(
+            title='Event 2',
+            description='Desc 2',
+            start_date='2003-01-01 10:00',
+            end_date='2003-01-01 12:00',
+            capacity=200,
+            place='tel-aviv'
+        )
+        test_event_2.save()
+        test_user1 = User.objects.create_user(
+            username='testuser1', password='Aa123123')
+
+        test_user1.save()
+
+        RecommendId = EventRecommend.objects.create(
+            EventId=2, user=test_user1, text="test")
+        RecommendId.save()
+
+        test_cancelled_event = CancelledEvent.objects.create(EventId=1)
+        test_cancelled_event.save()
+
+        test_event_update = EventUpdates.objects.create(
+            EventId=2, announcement='ann test')
+        test_event_update.save()
+
+        test_my_events = MyEvent.objects.create(EventId=2, user=User.objects.create(
+            username='test2', email='test2@email.com', first_name='Big2', last_name='Bob2'))
+        test_my_events.save()
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get(reverse('event-users', args=[1]))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_not_found(self):
+        response = self.client.get(reverse('event-users', args=[1000]))
+        self.assertEqual(response.status_code, 404)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('event-users', args=[1]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'event/users_attend.html')
